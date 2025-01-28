4d84047b9f7ed6095717b1c1b3c65da7a97d79fb
Ashluna22 <ashleyvera032@gmail.com>
2025-01-27 21:20:36 -0500

    Create hour model

---
+++ Capstone-BE/Models/HourlyModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace Capstone_BE.Models
+{
+    public class HourlyModel
+    {
+        //High Temps
+        [JsonPropertyName("tempmax")]
+        public double HighTemp { get; set; }
+
+        // Low Temp
+        [JsonPropertyName("tempmin")]
+        public double LowTemp { get; set; }
+
+        [JsonPropertyName("windspeed")]
+        public double WindSpeed { get; set; }
+
+        // Precipitation 
+        [JsonPropertyName("precipprob")]
+        public int Precipitation { get; set; }
+
+        //Feels Like
+        [JsonPropertyName("feelslike")]
+        public int FeelsLike { get; set; }
+       
+    }
+}
