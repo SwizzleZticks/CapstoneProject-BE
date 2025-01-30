67c04ef4a8dd17cfba322c9af13ad9742cb2c202
Ryan <ryansabon@gmail.com>
2025-01-29 21:38:37 -0500

    Added API endpoints for retrieving daily and hourly forecasts

--- Capstone-BE/Controllers/WeatherController.cs
+++ Capstone-BE/Controllers/WeatherController.cs
@@ -21,5 +21,18 @@ public class WeatherController : Controller
     {
         return Ok(await _service.GetSevenDayForecastAsync(location));
     }
-    
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<Day>> GetDayAsync([FromQuery] string location)
+    {
+        return Ok(await _service.GetDayForecastAsync(location));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<TimeLine>> GetHourAsync([FromQuery] string location)
+    {
+        return Ok(await _service.GetHourlyForecastAsync(location));
+    }
 }
