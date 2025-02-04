4ef3c4534025f250101fd85418a9c3554ddd8b27
Ashluna22 <ashleyvera032@gmail.com>
2025-02-03 20:09:26 -0500

    added location controller

--- Capstone-BE/Models/Location.cs
+++ Capstone-BE/Models/Location.cs
@@ -6,7 +6,11 @@ namespace Capstone_BE.Models;
 public partial class Location
 {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public int LocationId { get; set; }
+=======
+    public short LocationId { get; set; }  // Primary Key
+>>>>>>> Stashed changes
 =======
     public short LocationId { get; set; }  // Primary Key
 >>>>>>> Stashed changes
@@ -16,7 +20,11 @@ public partial class Location
     public string? State { get; set; }
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     public virtual ICollection<User> Users { get; set; } = new List<User>();
+=======
+    public virtual ICollection<User> Users { get; set; } = new List<User>();  // Relationships with Users
+>>>>>>> Stashed changes
 =======
     public virtual ICollection<User> Users { get; set; } = new List<User>();  // Relationships with Users
 >>>>>>> Stashed changes
