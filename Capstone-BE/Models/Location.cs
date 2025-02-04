9918da60f1548381fe33cae180946ab2da8e64b1
Ashluna22 <ashleyvera032@gmail.com>
2025-02-03 20:49:58 -0500

    Added location controller

--- Capstone-BE/Models/Location.cs
+++ Capstone-BE/Models/Location.cs
@@ -5,12 +5,11 @@ namespace Capstone_BE.Models;
 
 public partial class Location
 {
-    public int LocationId { get; set; }
+    public short LocationId { get; set; }
 
     public string? City { get; set; }
 
     public string? State { get; set; }
 
-    public virtual ICollection<User> Users { get; set; } = new List<User>();
 }
 
